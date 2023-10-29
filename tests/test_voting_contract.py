68ab4c80b53a70576067492b6c00debaae75250b
Giorgi Sharmiashvili <gsharmiashvili99@gmail.com>
2023-10-30 00:32:01 +0400

    add new test cases

--- tests/test_voting_contract.py
+++ tests/test_voting_contract.py
@@ -1,41 +1,79 @@
 from enum import Enum
+from datetime import datetime, timedelta
 
 import pytest
 from web3.exceptions import ContractLogicError
 
 from deploy import deploy_contract, public_address, web3
+from voting_contract.client import FruitContractClient
+from voting_contract.choices import Fruit
 
 
-contract = deploy_contract()
-
-
-class Fruit(Enum):
-    APPLE = 0
-    BANANA = 1
-    WATERMELON = 2
-    STRAWBERRY = 3
-
-
-def test_contract_voting():
-
-    tx_hash = contract.functions.voteForFruit(Fruit.APPLE.value).transact({
-        "from": public_address
-    })
-
-    web3.eth.wait_for_transaction_receipt(tx_hash)
-
-    assert contract.functions.fruitVotes(Fruit.APPLE.value).call() == 1
-    for fruit in Fruit:
-        if fruit.value != Fruit.APPLE.value:
-            assert contract.functions.fruitVotes(fruit.value).call() == 0
-
-    assert contract.functions.votedAddresses(public_address).call() is True
+def test_only_owner_can_change_auction_creation_date():
+    client = FruitContractClient(deploy_contract(), web3)
+    client.change_contract_creation_time(public_address, int(datetime.utcnow().timestamp()))
 
     with pytest.raises(ContractLogicError) as e:
-        tx_hash = contract.functions.voteForFruit(Fruit.APPLE.value).transact({
-            "from": public_address
-        })
+        client.change_contract_creation_time(web3.eth.accounts[1], int(datetime.utcnow().timestamp()))
 
-        web3.eth.wait_for_transaction_receipt(tx_hash)
+    assert "Only owner can perform this action" in str(e)
+
+
+def test_account_cannot_make_more_than_one_vote():
+    client = FruitContractClient(deploy_contract(), web3)
+
+    client.vote_for_fruit(public_address, Fruit.APPLE)
+
+    assert client.get_fruit_votes(Fruit.APPLE) == 1
+    for fruit in Fruit:
+        if fruit.value != Fruit.APPLE.value:
+            assert client.get_fruit_votes(fruit) == 0
+
+    assert client.account_already_voted(public_address) is True
+
+    with pytest.raises(ContractLogicError) as e:
+        client.vote_for_fruit(public_address, Fruit.APPLE)
 
     assert "Cannot make more than 1 vote" in str(e)
+
+
+def test_cannot_make_vote_when_it_is_ended():
+    client = FruitContractClient(deploy_contract(), web3)
+
+    new_timestamp = (datetime.utcnow() - timedelta(days=5)).timestamp()
+    client.change_contract_creation_time(public_address, int(new_timestamp))
+
+    with pytest.raises(ContractLogicError) as e:
+        client.vote_for_fruit(public_address, Fruit.APPLE)
+
+    assert "Voting has ended." in str(e)
+
+
+def test_cannot_get_winner_until_voting_is_ended():
+    client = FruitContractClient(deploy_contract(), web3)
+
+    with pytest.raises(ValueError) as e:
+        client.get_winner()
+
+    assert "Voting has not ended" in str(e)
+
+
+def test_correct_winner_is_calculated():
+    client = FruitContractClient(deploy_contract(), web3)
+
+    votes_for_fruits = {
+        Fruit.APPLE: 2,
+        Fruit.WATERMELON: 1,
+        Fruit.BANANA: 3
+    }
+
+    account_idx = 1
+    for fruit, num_votes in votes_for_fruits.items():
+        for _ in range(num_votes):
+            client.vote_for_fruit(web3.eth.accounts[account_idx], fruit)
+            account_idx += 1
+
+    new_timestamp = (datetime.utcnow() - timedelta(days=5)).timestamp()
+    client.change_contract_creation_time(public_address, int(new_timestamp))
+
+    assert client.get_winner() == Fruit.BANANA.value
